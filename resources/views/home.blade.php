d5cee9a4f596d7e37e2075d2e66b41abfd6a36c7
zhaysbayev <zhanysbaev.a123@gmail.com>
2021-02-20 23:37:31 +0600

    created other views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('home.css') }}">
     <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('home.css') }}">
 </head>
-<body>
+<body class = "bg-dark">
+<div class= "header">
+           <a href="" id="home">Home</a>
+           <a href="{{ route('about') }}" id="about" >About</a>
+           <a href="{{ route('contact') }}" id="contact">Contact</a>
+</div>
     
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,10 @@ Route::get('/', function () {
 });
 Route::get('/home', function () {
     return view('home');
-});
+})->name('home');
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
